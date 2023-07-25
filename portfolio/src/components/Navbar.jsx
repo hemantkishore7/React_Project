8afdb9f692ccb1a8cdc03b53dda9bf689a2095e7
hemantkishore7 <hemantkishore98@gmail.com>
2023-07-25 23:25:22 +0530

    ups

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaBars, FaTimes, FaLinkedin, FaGithub } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
-import resume from "../assets/resume/resume.pdf"
+import resume from "../assets/resume/resume.pdf";
 
 function Navbar() {
   const [nav, setNav] = useState(false);
@@ -30,17 +30,24 @@ function Navbar() {
             About
           </Link>
         </li>
+
+        <li>
+          {" "}
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
         <li>
           <Link to="project" smooth={true} duration={500}>
             Project
           </Link>
         </li>
-        <li> <Link to='skills' smooth={true} duration={500}>
-            Skills
-          </Link></li>
-        <li> <Link to='contact' smooth={true} duration={500}>
+        <li>
+          {" "}
+          <Link to="contact" smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger */}
@@ -56,21 +63,32 @@ function Navbar() {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to="home" smooth={true} duration={500}>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to="about" smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
-          </Link></li>
-          <li className="py-6 text-4xl"><Link onClick={handleClick} to="project" smooth={true} duration={500}>
-            Project
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
-          </Link></li>
-        <li className="py-6 text-4xl"><Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="project" smooth={true} duration={500}>
+            Project
+          </Link>
+        </li>
+        
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
-          </Link></li>
+          </Link>
+        </li>
       </ul>
 
       {/* social media */}
@@ -106,7 +124,8 @@ function Navbar() {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href={resume} download="Resume"
+              href={resume}
+              download="Resume"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
